049e496d429fd562114ffbf89ac72bd15463e276
DESKTOP-6T8U07T\User <mohamed.hamdy.issa@gmail.com>
2022-07-21 13:40:19 +0200

    take Screenshots

--- src/test/java/testing/MaxWindowDeleteCookies.java
+++ src/test/java/testing/MaxWindowDeleteCookies.java
@@ -1,10 +1,14 @@
 package testing;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+
+
+import org.openqa.selenium.chrome.ChromeDriver; 
 
 public class MaxWindowDeleteCookies {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		
 		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
@@ -17,7 +21,6 @@ public class MaxWindowDeleteCookies {
 		
 		
 
-		
 		
 
 	}
---
+++ src/test/java/testing/TakeScreenshot.java
@@ -0,0 +1,27 @@
+package testing;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TakeScreenshot {
+
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+		
+
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(src, new File("C:\\Users\\User\\Downloads\\screenshots\\screenshots.png"));
+	}
+
+}
