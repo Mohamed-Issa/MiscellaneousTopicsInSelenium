ba026a99d9a68cdbc14792df74047c3f1005bad5
DESKTOP-6T8U07T\User <mohamed.hamdy.issa@gmail.com>
2022-07-21 12:05:35 +0200

    Explore chrome options to set proxies, plugins & paths on chrome

--- src/test/java/testing/HTTPsCertifications.java
+++ src/test/java/testing/HTTPsCertifications.java
@@ -1,27 +1,43 @@
 package testing;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class HTTPsCertifications {
 
 	public static void main(String[] args) {
-		
-		
+
 		ChromeOptions options = new ChromeOptions();
 		options.setAcceptInsecureCerts(true);
-		
-		
+
+		options.addExtensions();
+		Proxy proxy = new Proxy();
+		proxy.setHttpProxy("ipaddress:4444");
+		options.setCapability("proxy", proxy);
+
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("download.default_directory", "/directory/path");
+		options.setExperimentalOption("prefs", prefs);
+
+		FirefoxOptions options2 = new FirefoxOptions();
+		options2.setAcceptInsecureCerts(true);
+
+		EdgeOptions options3 = new EdgeOptions();
+		options3.setAcceptInsecureCerts(true);
+
 		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
-		
+
 		ChromeDriver driver = new ChromeDriver(options);
-		
+
 		driver.get("https://expired.badssl.com/");
-		
+
 		System.out.println(driver.getTitle());
-		
-		
 
 	}
 
