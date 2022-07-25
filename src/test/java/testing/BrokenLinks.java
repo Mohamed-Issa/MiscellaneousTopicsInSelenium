baa9e24d850e34d95c0a15d508d11250e4ab63c6
DESKTOP-6T8U07T\User <mohamed.hamdy.issa@gmail.com>
2022-07-25 13:20:44 +0200

    iterate over all links in the page to validate broken links mechanism

--- src/test/java/testing/BrokenLinks.java
+++ src/test/java/testing/BrokenLinks.java
@@ -1,13 +1,15 @@
 package testing;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class BrokenLinks {
 
@@ -15,19 +17,28 @@ public class BrokenLinks {
 		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
 
 		ChromeDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		// driver.manage().window().maximize();
 
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
-		String url = driver.findElement(By.cssSelector("a[href*='brokenlink'")).getAttribute("href");
+		List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
 
-		System.out.println(url);
+		// System.out.println(url);
 
-		HttpsURLConnection conn = (HttpsURLConnection) new URL(url).openConnection();
-		conn.setRequestMethod("HEAD");
-		conn.connect();
-		int responseCode = conn.getResponseCode();
-		System.out.println(responseCode);
+		for (WebElement link : links) {
+
+			String url = link.getAttribute("href");
+			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+			conn.setRequestMethod("HEAD");
+			conn.connect();
+			int responseCode = conn.getResponseCode();
+			System.out.println(responseCode);
+
+			if (responseCode > 400) {
+				System.out.println("The link with Text " + link.getText() + " is broken with the code " + responseCode);
+				Assert.assertTrue(false);
+			}
+		}
 
 	}
 
